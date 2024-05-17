12fa9d26ee1c0a01b0e03a667a8b0c0eb97d5c90
HadikaMalik <hadika.malik@live.com>
2024-05-18 00:14:49 +0100

    ticket delete endpoint done

--- server/api.js
+++ server/api.js
@@ -3,8 +3,30 @@ import db from "./db.js";
 const router = Router();
 
 router.get("/videos", async (_, res) => {
-	const result = await db.query("SELECT * FROM videos;");
-	res.json(result.rows);
-});
+	try{
+		const result = await db.query("SELECT * FROM videos;");
+		res.json(result.rows);
+	}
+	catch(error){
+		console.error("Database connection error:", error);
+		res.status(500).json({success:false, error: "Could not connect to the database"});
+	}
+}); 
+
+router.delete("/api/videos/:id", async (req, res) => {
+	const videoId = req.params.id;
+	try{
+		const checkResult = await db.query("SELECT * FROM videos WHERE id = $1;", [videoId]);
+		if (checkResult.rows.length === 0){
+			return res.status(404).json({success: false, error: "Video not found"});
+		}
+	await db.query("DELETE FROM videos WHERE id = $1", [videoId]);
+	res.status(204).send();
+	}
+	catch(error){
+		console.error("Databse connection error:", error);
+		res.status(500).json({success: false, error: "Could not delete the video"});
+	}
+})
 
 export default router;
